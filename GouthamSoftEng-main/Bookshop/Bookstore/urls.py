09d65c9b654f8fbc5ad282f87056a51ff49d37df
Dhanushkumar04 <dkdhanush1970@gmail.com>
2024-05-21 16:00:18 +0530

    test

--- GouthamSoftEng-main/Bookshop/Bookstore/urls.py
+++ GouthamSoftEng-main/Bookshop/Bookstore/urls.py
@@ -7,10 +7,12 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name="login"),
     path('signup/',views.signup, name="signup"),
+    path('about/',views.about, name="about"),
     path('',views.home, name="home"),
     path('logout/',views.logout,name="logout"),
     path('cart/', views.view_cart, name='view_cart'),
-    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    
+    path('add/<str:product_id>/', views.add_to_cart, name='add_to_cart'),   
     path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('contact/',views.contact, name='contact'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- GouthamSoftEng-main/Bookshop/Bookstore/views.py
+++ GouthamSoftEng-main/Bookshop/Bookstore/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, CartItem
 from django.contrib.auth.decorators import login_required
 from Bookstore.urls import *
@@ -41,6 +41,7 @@ def signup(request):
                 user.save()
                 return redirect('login')
             else :
+                messages.info(request, "Password and Confirm Password is not matching")
                 return redirect('signup')   
         else:
             return render(request, "signup.html")
@@ -54,35 +55,56 @@ def logout(request):
 
 def home(request):
     products = Product.objects.all()
-    if request.user.is_authenticated:
+    if request.user.is_authenticated:              
         cart_items = CartItem.objects.filter(user=request.user)
         count = cart_items.count()
-        return render(request, 'home.html', {'products': products, 'count':count})
+        return render(request, 'home.html', {'products': products, 'number':count})
     return render(request, 'home.html', {'products': products})
  
 def view_cart(request):
     cart_items = CartItem.objects.filter(user=request.user)
+    products = Product.objects.all()
     count = cart_items.count()
     total_price = sum(item.product.price * item.quantity for item in cart_items)
-    return render(request, 'cart.html', {'cart_items': cart_items, 'total_price': total_price, 'count': count})
+    return render(request, 'cart.html', {'cart_items': cart_items, 'total_price': total_price, 'number': count, 'products':products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'description.html', {'product': product})
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
-    cart_item, created = CartItem.objects.get_or_create(product=product, user=request.user)
-    cart_item.quantity += 1
-    cart_item.save()
-    return redirect("view_cart")
+    if request.method == "POST":
+        count = request.POST.get("count")
+        if int(count) <= int(product.quantity) and int(product.quantity) > 0:
+            product.quantity = int(product.quantity) - int(count)
+            product.save()
+            cart_item, created = CartItem.objects.get_or_create(product=product, user=request.user)
+            cart_item.quantity += int(count)
+            cart_item.save()
+            return redirect("home")
+        else:
+            messages.warning(request,"You Given Out of Stocks.")
+            return redirect('home')
 
 def remove_from_cart(request, item_id):
     cart_item = CartItem.objects.get(id=item_id)
-    cart_item.delete()
+    product = Product.objects.get(id = int(cart_item.product.id))
+    print(product)
+    if product.name == cart_item.product.name: 
+        print(product.quantity)
+        product.quantity = int(product.quantity) + int(cart_item.quantity)
+        print(product.quantity)
+        product.save()
+        cart_item.delete()
     return redirect("view_cart")
 
 def view_desc(request):
     return render(request,'description.html')
 
 
-
+def about(request):
+    return render(request, 'about.html')
     
-
-
+def contact(request):
+    return render(request, 'contact.html')
